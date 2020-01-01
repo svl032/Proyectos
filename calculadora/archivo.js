fcfa48480eaef768b7041d3dc7cbdc7b7b9b7033
comangelo <acomuniello@gmail.com>
2020-01-01 15:31:43 +0000

    igualok

--- calculadora/archivo.js
+++ calculadora/archivo.js
@@ -1,14 +1,16 @@
 var pantalla = 0;
 var residuo = 0;
 var accion = "";
+var esResultado = false;
 
 function uno() {
-  if (pantalla == 0) {
+  if (pantalla == 0 || esResultado) {
     pantalla = 1;
   } else {
     pantalla = parseInt(pantalla + "1");
   }
 
+  esResultado = false;
   console.log(pantalla, residuo);
 }
 
@@ -31,5 +33,6 @@ function igual() {
     residuo = 0;
     accion = "";
   }
+  esResultado = true;
   console.log(pantalla, residuo);
 }
