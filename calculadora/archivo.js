e316ace6b4583819b900891dd9aeeaf1a0db2350
comangelo <acomuniello@gmail.com>
2020-01-01 12:32:48 +0000

    funcion suma y funcion igual

--- calculadora/archivo.js
+++ calculadora/archivo.js
@@ -1,5 +1,30 @@
-function uno() {
-  var valorActual = document.getElementById("resultado").innerHTML;
+var pantalla = 0;
+var residuo = 0;
+var accion = "";
 
-  document.getElementById("resultado").innerHTML = parseInt(valorActual) + 1;
+function uno() {
+  if (pantalla == 0) {
+    pantalla = 1;
+  } else {
+    pantalla = parseInt(pantalla + "1");
+  }
+
+  console.log(pantalla, residuo);
+}
+
+function suma() {
+  residuo = pantalla;
+  pantalla = 0;
+  accion = "suma";
+
+  console.log(pantalla, residuo);
+}
+
+function igual() {
+  if (accion == "suma") {
+    pantalla = pantalla + residuo;
+    residuo = 0;
+    accion = "";
+  }
+  console.log(pantalla, residuo);
 }
